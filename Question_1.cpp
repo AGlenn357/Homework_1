b40106c324845c985b519b0f69d46dac3f5bf9c4
Andrew Glenn <andrew_glenn@MacBook-Pro.local>
2023-09-15 23:04:26 -0400

    README

---
+++ Question_1.cpp
@@ -0,0 +1,15 @@
+/*
+Andrew Glenn
+AERSP 424
+Homework 1
+Question 1: Determining the roll rate of an aircraft
+*/
+
+#include <iostream> //include input output library
+#include <string> //include string library
+
+using namespace std; //using standard library
+
+int main() {
+    return 0;
+}
