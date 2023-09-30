86fed05a777a2b3cb14b9e1f3ee37446b3a89630
Andrew Glenn <andrew_glenn@MacBook-Pro.local>
2023-09-30 18:02:17 -0400

    comment changes

--- Question_1.cpp
+++ Question_1.cpp
@@ -15,7 +15,7 @@ int main() {
     float Lp = 0.33; //Lp value of plane
     float Ldelta = -0.75; //Ldelta value of plane
     double Pt1; //roll rate at time t+1
-    int delta; //elevator deflection value
+    int delta; //aileron deflection value
     double Pt = 1; //roll rate at time t
     double P_dot; //time derivative of roll rate
     double time; //time variable
@@ -23,16 +23,17 @@ int main() {
     
     for (int i = 1; i <= 500; i++) { //loop from t = 0 to t = 5
         if (i <= 200) {
-            delta = 1; //ailerion delfection value when t <= 2 s
+            delta = 1; //aileron delfection value when t <= 2 s
         }
         else {
-            delta = 0; //ailerion delfection value when t > 2 s
+            delta = 0; //aileron delfection value when t > 2 s
         }
         P_dot = Lp*Pt+Ldelta*delta; //finding roll rate derivative value
-        Pt1 = Pt+P_dot*0.01; //new roll value at time t+1
+        Pt1 = Pt+P_dot*0.01; //new roll rate value at time t+1
         Pt = Pt1; //setting initial roll rate to current roll rate (for next iteration)
         time = i*0.01; //time of specific iteration
         cout << "Time: " << fixed << setprecision(2) << time << " s Roll Rate: " << Pt1 << endl; //outputting time and roll rate
     }
     return 0;
 }
+
