33128d94b873b85578aad0505c9dc49d3cdb5217
Andrew Glenn <andrew_glenn@MacBook-Pro.local>
2023-09-30 22:06:57 -0400

    question 6 solution

--- Question_6.cpp
+++ Question_6.cpp
@@ -4,3 +4,22 @@ Homework 1
 Question 6: creating a gradient cost function
 */
 
+#include <vector> //include vector library
+#include <iostream> //include input ourput library
+#include <iomanip> //include input output manipulation library
+#include <math.h> //including math library
+
+using namespace std; //using standard library
+
+double gradient_cost(double y_predict, double y ) { //gradient cost function
+    double result = 2*(y_predict - y); //finding gradient cost function
+    return result; //returning result
+}
+
+int main() {
+    double y_predict = 0.504421; //y prediction value
+    double y = 1; //y value
+    double answer = gradient_cost(y_predict, y); //calling gradient cost function
+    cout << "The gradient cost is " << answer << endl; //outputting the gradient cost
+}
+
